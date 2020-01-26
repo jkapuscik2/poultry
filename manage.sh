db83998d0998059772841dbe8747124221847345
kuba <j.kapuscik@abyssglass.pl>
2020-01-26 15:28:32 +0100

    Upgraded manage script

--- manage.sh
+++ manage.sh
@@ -34,23 +34,51 @@ then
 fi
 
 
-if [ -z "$2" ]
+if [ -z "$3" ]
 then
     echo "KeyName must be provided as third argument"
     exit 1
 fi
 
+DatabaseName=$4
+DefaultDatabaseName="PoultryDB"
+if [ -z "$DatabaseName" ]
+then
+    DatabaseName=$DefaultDatabaseName
+    echo "DatabaseName not provided. Using default '$DefaultDatabaseName'"
+fi
+DatabaseUsername=$5
+DefaultDatabaseUsername="admin"
+if [ -z "$DatabaseUsername" ]
+then
+     DatabaseUsername=$DefaultDatabaseUsername
+    echo "DatabaseUsername not provided. Using default '$DefaultDatabaseUsername'"
+fi
+DatabaseUserPassword=$6
+DefaultDatabaseUserPassword="admin123456"
+if [ -z "$DatabaseUserPassword" ]
+then
+     DatabaseUserPassword=$DefaultDatabaseUserPassword
+    echo "DatabaseUserPassword not provided. Using default '$DefaultDatabaseUserPassword'"
+fi
+BucketName=$6
+DefaultBucketName="eggs-eggs-1234"
+if [ -z "$BucketName" ]
+then
+     BucketName=$DefaultBucketName
+    echo "BucketName not provided. Using default '$DefaultBucketName'"
+fi
+
+
 if [  "$1" == "create" ]
 then
     echo "Creating stack: $2"
-    aws cloudformation create-stack --stack-name "$2" --template-body file://infrastructure/base.yml --capabilities CAPABILITY_IAM --parameters ParameterKey=KeyName,ParameterValue="$3"
-    #  ParameterKey=DatabaseName,ParameterValue="$4" ParameterKey=DatabaseUsername,ParameterValue="$5" ParameterKey=DatabaseUserPassword,ParameterValue="$6" ParameterKey=BucketName,ParameterValue="$7"
+    aws cloudformation create-stack --stack-name "$2" --template-body file://infrastructure/base.yml --capabilities CAPABILITY_IAM --parameters ParameterKey=KeyName,ParameterValue="$3" ParameterKey=DatabaseName,ParameterValue="$DatabaseName" ParameterKey=DatabaseUsername,ParameterValue="$DatabaseUsername" ParameterKey=DatabaseUserPassword,ParameterValue="$DatabaseUserPassword" ParameterKey=BucketName,ParameterValue="$BucketName"
 fi
 
 if [  "$1" == "update" ]
 then
     echo "Updating stack: $2"
-    aws cloudformation update-stack --stack-name "$2" --template-body file://infrastructure/base.yml --capabilities CAPABILITY_IAM --parameters ParameterKey=KeyName,ParameterValue="$3"
-    # ParameterKey=DatabaseName,ParameterValue="$4" ParameterKey=DatabaseUsername,ParameterValue="$5" ParameterKey=DatabaseUserPassword,ParameterValue="$6" ParameterKey=BucketName,ParameterValue="$7"
+    aws cloudformation update-stack --stack-name "$2" --template-body file://infrastructure/base.yml --capabilities CAPABILITY_IAM --parameters ParameterKey=KeyName,ParameterValue="$3" ParameterKey=DatabaseName,ParameterValue="$DatabaseName" ParameterKey=DatabaseUsername,ParameterValue="$DatabaseUsername" ParameterKey=DatabaseUserPassword,ParameterValue="$DatabaseUserPassword" ParameterKey=BucketName,ParameterValue="$BucketName"
 fi
 
