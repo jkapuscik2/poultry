59cb45b16f557ca80c57a0eefc83dd3f2b402315
kuba <j.kapuscik@abyssglass.pl>
2020-02-10 21:10:12 +0100

    Added permissions setting after deployment

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 sudo apt-get install -y php7.3 php7.3-fpm php7.3-common php7.3-mysql php7.3-gd php7.3-xml php7.3-mbstring php7.3-zip
-composer install
+
+chown www-data:www-data /var/www -R
+sudo find /var/www/poultry -type f -exec chmod 644 {} \;
+sudo find /var/www/poultry -type d -exec chmod 755 {} \;
+sudo chown -R www-data:www-data /var/www/poultry/vendor
+sudo chown -R www-data:www-data /var/www/poultry/storage
+
 cd /var/www/poultry
-composer install
+composer install --optimize-autoloader --no-dev
 php artisan migrate
