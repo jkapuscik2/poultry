1fad008a035fa259ac123375638f83f69e4ffa07
kuba <j.kapuscik@abyssglass.pl>
2020-02-10 20:42:39 +0100

    Added cleanup script

---
+++ scripts/cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rf /var/www/poultry/app/
+rm -rf /var/www/poultry/bootstrap/
+rm -rf /var/www/poultry/database/
+rm -rf /var/www/poultry/public/
+rm -rf /var/www/poultry/resources/
+rm -rf /var/www/poultry/routes/
+rm -rf /var/www/poultry/scripts/
+rm -rf /var/www/poultry/storage/
+rm -rf /var/www/poultry/tests/
+rm /var/www/poultry/artisan
+rm /var/www/poultry/composer.json
+rm /var/www/poultry/composer.lock
+rm /var/www/phplaravel/phpunit.xml
